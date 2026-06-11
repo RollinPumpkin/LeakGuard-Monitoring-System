d8372f7809396c79543769bf7f998140ad66731a
Febilid <septapuma@gmail.com>
2026-06-11 23:12:30 +0700

    route esp32

---
+++ web/app/api/sensor/route.ts
@@ -0,0 +1,35 @@
+import { supabase } from "@/lib/supabase";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { error } = await supabase
+      .from("sensor_readings")
+      .insert([body]);
+
+    if (error) {
+      console.error(error);
+
+      return Response.json(
+        {
+          success: false,
+          error: error.message
+        },
+        { status: 500 }
+      );
+    }
+
+    return Response.json({
+      success: true
+    });
+
+  } catch (err) {
+    return Response.json(
+      {
+        success: false
+      },
+      { status: 500 }
+    );
+  }
+}
