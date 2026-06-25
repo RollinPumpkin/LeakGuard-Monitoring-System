90d56062912eeab95a3ab95227bb19e4ba4108fc
Febilid <septapuma@gmail.com>
2026-06-26 03:10:01 +0700

    fix login rooute

--- web/components/Sidebar.tsx
+++ web/components/Sidebar.tsx
@@ -41,8 +41,7 @@ export function Sidebar() {
 
   const handleLogout = async () => {
     await supabase.auth.signOut()
-    router.push('/login')
-    router.refresh()
+    window.location.href = '/login'
   }
 
   useEffect(() => {
